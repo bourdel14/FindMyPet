b3100eeabea1673af8fc35aee96989e076a36118
tbourdel <theo.bourdel@gmail.com>
2018-02-13 14:37:54 +0100

    Ajout des classes user,role et type_animal

---
+++ FindMyPet/FindMyPet/Models/Role.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FindMyPet.Models
+{
+    public class Role
+    {
+        public int id { get; set; }
+        public String libelle { get; set; }
+
+        public Role()
+        {
+
+        }
+    }
+}
--- FindMyPet/FindMyPet/Models/Utilisateur.cs
+++ FindMyPet/FindMyPet/Models/Utilisateur.cs
@@ -7,5 +7,18 @@ namespace FindMyPet.Models
 {
     public class Utilisateur
     {
+        public int id { get; set; }
+        public String nom { get; set; }
+        public String prenom { get; set; }
+        public String email { get; set; }
+        public String login { get; set; }
+        public String password { get; set; }
+        public virtual Role role_id {get;set; }
+
+        public Utilisateur()
+        {
+
+        }
+      
     }
 }
