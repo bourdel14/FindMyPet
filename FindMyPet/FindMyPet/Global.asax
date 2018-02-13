115b001998aef1befb91a75e3ffbbcfbcbfcedf1
abattais <alexandre-bat@hotmail.fr>
2018-02-13 11:34:07 +0100

    test push solution

---
+++ FindMyPet/FindMyPet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FindMyPet.MvcApplication" Language="C#" %>
---
+++ FindMyPet/FindMyPet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FindMyPet.Startup))]
+namespace FindMyPet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
